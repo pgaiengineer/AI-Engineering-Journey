dbd6e96df435868d96b841d5474c15db3a8556fb
pgaiengineer <pg.ai.engineer@gmail.com>
2026-04-18 22:07:29 -0400

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,8 @@
+# This is a comment. It's for humans, not the computer.
+name = "Gemini" # String
+age = 46        # Integer
+height_ft = 6.1 # Float
+is_learning = True  # Boolean
+
+# Printing combines these into a sentence
+print(f"My name is {name} and I am {age} years old.")
