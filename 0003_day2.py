fd88b355697f1367ab00cae932eedc928de8e9ac
pgaiengineer <pg.ai.engineer@gmail.com>
2026-04-19 00:07:03 -0400

    Add files via upload

---
+++ 0003_day2.py
@@ -0,0 +1,9 @@
+temperature = 28  # Degrees Celsius
+
+if temperature > 25:
+    print("It is hot.")
+    print("Turn on AC.")
+else:
+    print("The temperature is fine.")
+
+print("This line always prints because it isn't indented.")
